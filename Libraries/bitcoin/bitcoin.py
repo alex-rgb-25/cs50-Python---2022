22101ff828c7fdc5dbd3cbb28db1d60990fd289f
Alexandru Simionescu <61823517+alex-rgb-25@users.noreply.github.com>
2022-10-30 11:42:02 +0200

    Create bitcoin.py

---
+++ Libraries/bitcoin/bitcoin.py
@@ -0,0 +1,17 @@
+import sys
+import requests
+
+if len(sys.argv) != 2:
+    sys.exit("Missing command-line argument")
+
+try:
+    n = float(sys.argv[1])
+except ValueError:
+    sys.exit("Command-line argument is not a number")
+
+response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+
+curValue = float(response.json()["bpi"]["USD"]["rate_float"])
+
+total = curValue * n
+print(f"${total:,.4f}")
